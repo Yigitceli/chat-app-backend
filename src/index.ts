709dfc26c86da77d6ae9b3d760dc21de0c54f4b4
Yigitceli <Yigit.celi@outlook.com>
2022-02-21 03:15:33 +0300

    first init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
 import cors from "cors";
-import 'dotenv/config'
-
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res, next) => {
   res.send(process.env.TEST);
